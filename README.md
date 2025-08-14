01c058f435c72b4c8d971ba038a2a0b57b4ac06d
Mark Honeychurch <mark@honeychurch.org>
2025-08-15 00:55:07 +1200

    Starting to save settings

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # WOW & Flutter Machine Config Generator
 
-This app
+This web app allows users to create and download a custom config for their Wow & Flutter machine. It features a handy visual selector to display dialog boxes that allow configuration of the settings connected to each part of the Wow Machine, as well as a config uploader
+
+The app also includes an audio file converter that converts audio files to 16-bit 48kHz WAV format, which is recommended for the Wow & Flutter machine.
+
+## Technical Details
+
+This app uses Petite Vue to enable dynamic elements, and is styled using Pico.css. FFMPEG.wasm is used to convert audio files to WAV format inside the browser.
--- petite-vue.js
+++ petite-vue.js
@@ -1,15 +1,11 @@
 import {createApp, reactive} from 'https://unpkg.com/petite-vue?module';
 // import {createApp, reactive} from 'https://unpkg.com/petite-vue/dist/petite-vue.es.js';
-// import {FFmpeg} from 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg/dist/esm/index.js';
-// import {FFmpeg} from 'https://unpkg.com/@ffmpeg/ffmpeg/dist/esm/index.js';
-// import {FFmpeg} from 'https://esm.sh/@ffmpeg/ffmpeg';
 import {createFFmpeg} from 'https://esm.sh/@ffmpeg/ffmpeg@0.11.6';
-// import {toBlobURL} from 'https://cdn.jsdelivr.net/npm/@ffmpeg/util/dist/esm/index.js';
 import settings from './settings.json' with {type: 'json'};
 
 for (const value of Object.values(settings)) value.value = value.default;
 
-const store = reactive({
+/* const store = reactive({
 	settings,
 	show: null,
 	colour: 'chilli',
@@ -17,55 +13,62 @@ const store = reactive({
 	comments: false,
 	faderSeparation: 100,
 	linkPitch: false,
+}); */
+
+const slider = (name) => ({
+	$template: '#slider',
+	name,
+	min: settings[name].values[0] || 0,
+	max: settings[name].values[1] || 100,
+	step: settings[name].values[2] || 1,
+	label: settings[name].label,
+	tooltip: settings[name].tooltip,
+	prefix: settings[name].prefix || '',
+	suffix: settings[name].suffix || '',
+	invert: settings[name].invert || false,
 });
 
-function slider(name) {
-	return {
-		$template: '#slider',
-		name,
-		min: settings[name].values[0] || 0,
-		max: settings[name].values[1] || 100,
-		step: settings[name].values[2] || 1,
-		label: settings[name].label,
-		tooltip: settings[name].tooltip,
-		prefix: settings[name].prefix || '',
-		suffix: settings[name].suffix || '',
-		invert: settings[name].invert || false,
-	};
-}
+const select = (name) => ({
+	$template: '#select',
+	name,
+	values: settings[name].values,
+	label: settings[name].label,
+	tooltip: settings[name].tooltip,
+});
 
-function select(name) {
-	return {
-		$template: '#select',
-		name,
-		values: settings[name].values,
-		label: settings[name].label,
-		tooltip: settings[name].tooltip,
-	};
-}
+const boolean = (name) => ({
+	$template: '#switch',
+	name,
+	label: settings[name].label,
+	tooltip: settings[name].tooltip,
+});
 
-function boolean(name) {
-	return {
-		$template: '#switch',
-		name,
-		label: settings[name].label,
-		tooltip: settings[name].tooltip,
-	};
-}
-
-function dialog_header(title) {
-	return {
-		$template: '#dialog_header',
-		title,
-	};
-}
+const dialog_header = (title) => ({
+	$template: '#dialog_header',
+	title,
+});
 
 createApp({
-	store,
 	slider,
 	select,
 	boolean,
 	dialog_header,
+	preferences: {
+		colour: 'chilli',
+		looper: true,
+		comments: false,
+		faderSeparation: 100,
+		audio_dialog: false,
+		config_dialog: false,
+	},
+	// store,
+	settings,
+	show: null,
+	colour: 'chilli',
+	looper: true,
+	comments: false,
+	faderSeparation: 100,
+	linkPitch: false,
 	prefix: '[wowmachine]\n',
 	colours: ['chilli', 'midnight', 'gold', 'silver'],
 	themes: ['red', 'slate', 'amber', 'grey'],
@@ -76,17 +79,35 @@ createApp({
 		reverb: false,
 	},
 	audio: new Audio(),
-	configDownloadDialog: false,
-	audioDownloadDialog: true,
+	dialogs: {
+		config: false,
+		audio: true,
+	},
 	toggle(name) {
-		this.store.show = this.store.show === name ? null : name;
+		this.show = this.show === name ? null : name;
 	},
 	nextColour() {
-		this.store.colour = this.colours[this.colours.indexOf(this.store.colour) + 1] || this.colours[0];
+		this.colour = this.colours[this.colours.indexOf(this.colour) + 1] || this.colours[0];
+	},
+	savePreferences() {
+		// Save preferences to local storage
+		localStorage.setItem('preferences', JSON.stringify(this.preferences));
+	},
+	loadPreferences() {
+		// Load preferences from local storage
+		const preferences = localStorage.getItem('preferences');
+		if (preferences) this.preferences = JSON.parse(preferences);
+	},
+	loadSettings() {
+		// Load settings from URL
+		
+	},
+	saveSettings() {
+		// Save settings to the URL
 	},
 	updateTheme() {
 		const link = document.querySelector('link[href^="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico"]');
-		link.href = `https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.${this.themes[this.colours.indexOf(this.store.colour)]}.min.css`;
+		link.href = `https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.${this.themes[this.colours.indexOf(this.colour)]}.min.css`;
 	},
 	playWAV(path) {
 		// If the audio is already playing, stop it
@@ -106,60 +127,60 @@ createApp({
 		return word.charAt(0).toUpperCase() + word.slice(1);
 	},
 	adjustFader(side) {
-		const settings = this.store.settings;
+		const settings = this.settings;
 		if (settings.fader_left.value === 'none' || settings.fader_right.value === 'none') return;
 		if (side === 'left') {
-			if (settings.left_cut_value.value > settings.right_cut_value.values[1] - this.store.faderSeparation) {
-				settings.left_cut_value.value = settings.right_cut_value.values[1] - this.store.faderSeparation;
+			if (settings.left_cut_value.value > settings.right_cut_value.values[1] - this.faderSeparation) {
+				settings.left_cut_value.value = settings.right_cut_value.values[1] - this.faderSeparation;
 			}
 			// When left slider moves, ensure right slider is at least buffer distance away
-			if (settings.left_cut_value.value + this.store.faderSeparation > settings.right_cut_value.value) {
-				settings.right_cut_value.value = settings.left_cut_value.value + this.store.faderSeparation;
+			if (settings.left_cut_value.value + this.faderSeparation > settings.right_cut_value.value) {
+				settings.right_cut_value.value = settings.left_cut_value.value + this.faderSeparation;
 			}
 		} else {
-			if (settings.right_cut_value.value < settings.left_cut_value.values[0] + this.store.faderSeparation) {
-				settings.right_cut_value.value = settings.left_cut_value.values[0] + this.store.faderSeparation;
+			if (settings.right_cut_value.value < settings.left_cut_value.values[0] + this.faderSeparation) {
+				settings.right_cut_value.value = settings.left_cut_value.values[0] + this.faderSeparation;
 			}
 			// When right slider moves, ensure left slider is at least buffer distance away
-			if (settings.right_cut_value.value - this.store.faderSeparation < settings.left_cut_value.value) {
-				settings.left_cut_value.value = settings.right_cut_value.value - this.store.faderSeparation;
+			if (settings.right_cut_value.value - this.faderSeparation < settings.left_cut_value.value) {
+				settings.left_cut_value.value = settings.right_cut_value.value - this.faderSeparation;
 			}
 		}
 	},
 	updatePitch(side) {
-		if (this.store.linkPitch) {
+		if (this.linkPitch) {
 			if (side === 'min') {
-				this.store.settings.pitch_range_max.value = this.store.settings.pitch_range_min.value;
+				this.settings.pitch_range_max.value = this.settings.pitch_range_min.value;
 			} else {
-				this.store.settings.pitch_range_min.value = this.store.settings.pitch_range_max.value;
+				this.settings.pitch_range_min.value = this.settings.pitch_range_max.value;
 			}
 		}
 	},
 	updateEffects() {
 		if (this.effects.delay) {
-			if (this.effects.reverb) this.store.settings.effect_mode.value = 'both';
-			else this.store.settings.effect_mode.value = 'delay';
+			if (this.effects.reverb) this.settings.effect_mode.value = 'both';
+			else this.settings.effect_mode.value = 'delay';
 		} else if (this.effects.reverb) {
-			this.store.settings.effect_mode.value = 'reverb';
+			this.settings.effect_mode.value = 'reverb';
 		}
 	},
 	sectionSettings(section) {
-		return Object.entries(this.store.settings).filter(([key, value]) => value.section === section).map(([key, {value}]) => `${key} = ${value}`).join('\n');
+		return Object.entries(this.settings).filter(([key, value]) => value.section === section).map(([key, {value}]) => `${key} = ${value}`).join('\n');
 	},
 	get config() {
-		if (this.store.comments) return this.prefix + '\n#In this file, do not delete any #.\n\n' + Object.entries(this.store.settings).map(([key, value]) => `# ${value.comment}\n${key} = ${value.value}`).join('\n\n');
-		return this.prefix + Object.entries(this.store.settings).map(([key, {value}]) => `${key} = ${value}`).join('\n');
+		if (this.comments) return this.prefix + '\n#In this file, do not delete any #.\n\n' + Object.entries(this.settings).map(([key, value]) => `# ${value.comment}\n${key} = ${value.value}`).join('\n\n');
+		return this.prefix + Object.entries(this.settings).map(([key, {value}]) => `${key} = ${value}`).join('\n');
 	},
 	reset() {
-		for (const key in this.store.settings) {
-			this.store.settings[key].value = this.store.settings[key].default;
+		for (const key in this.settings) {
+			this.settings[key].value = this.settings[key].default;
 		}
 	},
 	updateURL() {
 		const url = new URL(window.location.href);
-		url.searchParams.set('colour', this.store.colour);
-		url.searchParams.set('looper', this.store.looper);
-		url.searchParams.set('comments', this.store.comments);
+		url.searchParams.set('colour', this.colour);
+		url.searchParams.set('looper', this.looper);
+		url.searchParams.set('comments', this.comments);
 		window.history.replaceState({}, '', url.toString());
 	},
 	upload() {
@@ -175,7 +196,7 @@ createApp({
 				lines.forEach(line => {
 					const [key, value] = line.split('=');
 					if (key && value) {
-						this.store.settings[key].value = value;
+						this.settings[key].value = value;
 					}
 				});
 			}
@@ -210,7 +231,7 @@ createApp({
 		}
 	},
 	downloadConfig() {
-		this.download(this.config, 'text/plain', '.txt', 'Config file', this.configDownloadDialog);
+		this.download(this.config, 'text/plain', '.txt', 'Config file', this.dialogs.config);
 	},
 	async audioToWAV(e) {
 		const file = e.target.files[0];
@@ -227,7 +248,7 @@ createApp({
 		const output = file.name.replace(/\.[^.]+$/, '.wav');
 		await ffmpeg.run('-i', file.name, '-ar', '48000', '-ac', '2', '-f', 'wav', output);
 		const data = ffmpeg.FS('readFile', output);
-		this.download(data, 'audio/wav', '.wav', 'Audio file', this.audioDownloadDialog);
+		this.download(data, 'audio/wav', '.wav', 'Audio file', this.dialogs.audio);
 	},
 	async convertAudio() {
 		const fileInput = document.createElement('input');
